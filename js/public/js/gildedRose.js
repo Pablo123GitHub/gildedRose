73641b01957157fd2830cb9c3fe4b63a4a3f100f
Pablo <paul.ladiv123@gmail.com>
2017-11-28 22:47:21 +0000

    add several tests to check broken down methods in updateQual function

--- js/public/js/gildedRose.js
+++ js/public/js/gildedRose.js
@@ -20,58 +20,72 @@
           if (this.isQualityLessTh50(i)) {
             this.raiseQuality(i);
             if (!this.isNameNotBackStg(i)) {
-              this.raiseQualityIfLssThn(i, 6)
-              this.raiseQualityIfLssThn(i, 11)
+              this.raiseQualityIfDaysUp(i, 6)
+              this.raiseQualityIfDaysUp(i, 11)
             }
           }
         }
 
-        if (this.isSulfuras(i)) {
+        if (this.isNotSulfuras(i)) {
           this.checkItemSellIn(i) = this.checkItemSellIn(i) - 1;
         }
 
       };
     },
+
     lowerQuality: function(i) {
       return this._items[i]._quality = this._items[i]._quality - 1;
     },
-    isSulfuras: function(i) {
-      (this._items[i].name != 'Sulfuras, Hand of Ragnaros');
+
+    isNotSulfuras: function(i) {
+      return (this._items[i].name != 'Sulfuras, Hand of Ragnaros');
     },
+
     isQualityPositive: function(i) {
-      (this._items[i]._quality > 0);
+      return (this._items[i]._quality > 0);
     },
+
     isNameNotBrie: function(i) {
-      (this._items[i].name != 'Aged Brie');
+      return (this._items[i].name != 'Aged Brie');
     },
+
     isNameNotBackStg: function(i) {
-      this._items[i].name != 'Backstage passes to a TAFKAL80ETC concert';
+      return this._items[i].name != 'Backstage passes to a TAFKAL80ETC concert';
     },
+
     isQualityLessTh50: function(i) {
-      this._items[i]._quality < 50;
+      return this._items[i]._quality < 50;
     },
-    raiseQuality: function(i) {
-      this._items[i]._quality = this._items[i]._quality + 1
+
+    raiseQuality: function(i) {    
+      return this._items[i]._quality = this._items[i]._quality + 1
+
     },
+
     checkItemSellIn: function(i) {
-      this._items[i].sellIn
+      console.log("checkItemSellIng", this._items[i]._sellIn );
+      return this._items[i]._sellIn
     },
+
     isQualPositiveLowerIfSulfura: function(i) {
       if (this.isQualityPositive(i)) {
-        if (this.isSulfuras(i)) {
-          this.lowerQuality(i);
+        if (this.isNotSulfuras(i)) {
+        return  this.lowerQuality(i);
         }
       }
     },
-    raiseQualityifMoreTh50: function(i) {
+
+    raiseQualityIfLssthn50: function(i) {
       if (this.isQualityLessTh50(i)) {
-        this.raiseQuality(i);
+      return this.raiseQuality(i);
       }
     },
-    raiseQualityIfLssThn: function(i, days) {
+
+    raiseQualityIfDaysUp: function(i, days) {
       if (this.checkItemSellIn(i) < days) {
-        this.raiseQualityifMoreTh50(i);
+        return this.raiseQualityIfLssthn50(i);
       }
+      return this.raiseQualityIfLssthn50(i);
     }
 
   };
@@ -106,7 +120,7 @@
 //       this._items[i]._quality = 0;
 //     }
 //   } else {
-//     this.raiseQualityifMoreTh50(i);
+//     this.raiseQualityIfLssthn50(i);
 //   }
 // }
 //   return this._items;
--- js/spec/frontend/unit/gildedRoseSpec.js
+++ js/spec/frontend/unit/gildedRoseSpec.js
@@ -8,7 +8,7 @@ describe("Gilded Rose", function() {
   beforeEach(function() {
 
     oneItem = {
-      _name: "test",
+      _name: "Sulfuras, Hand of Ragnaros",
       _quality: 5,
       _sellIn: 3
 
@@ -29,9 +29,83 @@ describe("Gilded Rose", function() {
     });
 
     it("should lower quality of the item", function() {
-      console.log("hein", shop);
       expect(shop.lowerQuality(0)).toEqual(4);
     });
+
+    it("should have a quality above 0", function() {
+      expect(shop.isQualityPositive(0)).toBe(true);
+    });
+
+    it("name should not be Aged Brie", function() {
+      expect(shop.isNameNotBrie(0)).toBe(true);
+    });
+
+    it("name should not be BackStage passes....", function() {
+      expect(shop.isNameNotBackStg(0)).toBe(true);
+    });
+
+    it("Quality should be below 50", function() {
+      expect(shop.isQualityLessTh50(0)).toBe(true);
+    });
+
+    it("should raise quality by 1", function() {
+      expect(shop.raiseQuality(0)).toEqual(6);
+    });
+
+    it("checks item sellIn", function() {
+
+      expect(shop.checkItemSellIn(0)).toEqual(3);
+    });
+
+    it("quality is >0 and this is the Sulfuras item, quality should lower", function() {
+      expect(shop.isQualPositiveLowerIfSulfura(0)).toEqual(4);
+    });
+
+    it("raise quality if quality less than 50 ", function() {
+      expect(shop.raiseQualityIfLssthn50(0)).toEqual(6);
+    });
+
+    // it("raise quality by TWO if quality less than 50 and sellIn days < 6", function() {
+    //   var oneItem2;
+    //   var item2;
+    //   var items2;
+    //   var shop2;
+    //   oneItem2 = {
+    //     _name: "Sulfuras, Hand of Ragnaros",
+    //     _quality: 5,
+    //     _sellIn: 0
+    //
+    //   };
+    //
+    //   items2 = [oneItem2];
+    //   shop2 = new Shop(items2);
+    //
+    //
+    //   expect(shop2.raiseQualityIfDaysUp(0, 8)).toEqual(7);
+    // });
+
+    it("raise quality by ONE if quality less than 50 and sellIn days < 11", function() {
+      var oneItem3;
+      var item3;
+      var items3;
+      var shop3;
+      oneItem3 = {
+        _name: "Sulfuras, Hand of Ragnaros",
+        _quality: 5,
+        _sellIn: 8
+
+      };
+
+      items3 = [oneItem3];
+      shop3 = new Shop(items3);
+
+
+      expect(shop3.raiseQualityIfDaysUp(0, 8)).toEqual(6);
+    });
+
+
+
+
 
 
 
